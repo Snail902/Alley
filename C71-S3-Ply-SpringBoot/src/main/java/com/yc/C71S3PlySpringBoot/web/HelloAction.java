06528469dd60b5e4ed5ba31c129c809b1c621fc1
Snail902 <55265231+Snail902@users.noreply.github.com>
2019-12-15 20:27:07 +0800

    其实这是个blog

--- C71-S3-Ply-SpringBoot/src/main/java/com/yc/C71S3PlySpringBoot/web/HelloAction.java
+++ C71-S3-Ply-SpringBoot/src/main/java/com/yc/C71S3PlySpringBoot/web/HelloAction.java
@@ -1,8 +1,10 @@
 package com.yc.C71S3PlySpringBoot.web;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @RestController
 public class HelloAction {
--- C71-S3-Ply-SpringBoot/src/test/java/com/yc/C71S3PlySpringBoot/C71S3PlySpringBootApplicationTests.java
+++ C71-S3-Ply-SpringBoot/src/test/java/com/yc/C71S3PlySpringBoot/C71S3PlySpringBootApplicationTests.java
@@ -8,17 +8,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.yc.C71S3PlySpringBoot.bean.Category;
+import com.yc.C71S3PlySpringBoot.bean.UserBean;
 import com.yc.C71S3PlySpringBoot.dao.CategoryDao;
+import com.yc.C71S3PlySpringBoot.dao.UserMapper;
 
 @SpringBootTest
 class C71S3PlySpringBootApplicationTests {
 	@Resource
 	private CategoryDao cdao;
 	
+	@Resource
+	private UserMapper um;
 	@Test
 	void contextLoads() {
 		List<Category> list = cdao.selectAll();
 		System.out.println(list);
 	}
-
+	@Test
+	void contextLoadsu() {
+		List<UserBean> list = um.selectExample();
+		System.out.println(list);
+	}
 }
